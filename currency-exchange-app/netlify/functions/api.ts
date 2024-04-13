ab855afa9a1961a61a74f955c3d296ed7a5d1074
Michael Buchar <michaelbuchar0@gmail.com>
2024-04-13 15:46:45 +0200

    Change syntax - troubleshooting

--- currency-exchange-app/netlify/functions/api.ts
+++ currency-exchange-app/netlify/functions/api.ts
@@ -1,23 +1,17 @@
 // YOUR_BASE_DIRECTORY/netlify/functions/api.ts
 
-import express, { Router } from "express";
-import serverless from "serverless-http";
+import express, { Router } from 'express';
+import serverless from 'serverless-http';
 
 const api = express();
 console.log('api', api);
-
 const router = Router();
 router.use('/', (req, res, next) => {
     console.log('i am inside server');
     next();
 });
-exports.handler = async function(event, context) {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: "Hello World from serverless function!" })
-    };
-};
+router.get('/hello', (req, res) => res.send('Hello World!'));
 
-api.use("/api/", router);
+api.use('/api/', router);
 
 export const handler = serverless(api);
