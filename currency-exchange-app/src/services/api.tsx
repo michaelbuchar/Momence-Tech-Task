da2dfb0df46d53a9d60a3558ba07bf4cfc2eb24e
Michael Buchar <michaelbuchar0@gmail.com>
2024-04-13 14:20:28 +0200

    Changing config

--- currency-exchange-app/src/services/api.tsx
+++ currency-exchange-app/src/services/api.tsx
@@ -4,8 +4,13 @@ import express, { Router } from "express";
 import serverless from "serverless-http";
 
 const api = express();
+console.log('api', api);
 
 const router = Router();
+router.use('/', (req, res, next) => {
+    console.log('i am inside server');
+    next();
+});
 router.get("/hello", (req, res) => res.send("Hello World!"));
 
 api.use("/api/", router);
