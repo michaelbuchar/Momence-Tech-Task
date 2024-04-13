f8cd54b640e0e6c8f80748818d119413b3e23e77
Michael Buchar <michaelbuchar0@gmail.com>
2024-04-13 13:37:40 +0200

    Test with hello

--- currency-exchange-app/src/store/useCurrencyRates.tsx
+++ currency-exchange-app/src/store/useCurrencyRates.tsx
@@ -2,7 +2,7 @@ import {useQuery} from 'react-query';
 
 const fetchCurrencyRates = async () => {
     let headers = new Headers();
-    const response = await fetch('https://www.cnb.cz/en/financial-markets/foreign-exchange-market/central-bank-exchange-rate-fixing/central-bank-exchange-rate-fixing/daily.txt', // use backend server
+    const response = await fetch('/api/hello', // use backend server
         {headers: headers});
     return await response.text();
 };
