b8d722097c5eb9582e1d1bc41ca450e7490068c2
Michael Buchar <michaelbuchar0@gmail.com>
2024-04-13 04:39:25 +0200

    Change API endpoint for production

--- currency-exchange-app/src/store/useCurrencyRates.tsx
+++ currency-exchange-app/src/store/useCurrencyRates.tsx
@@ -2,7 +2,7 @@ import {useQuery} from 'react-query';
 
 const fetchCurrencyRates = async () => {
     let headers = new Headers();
-    const response = await fetch('http://localhost:5001/currency-rates', // use backend server
+    const response = await fetch('https://www.cnb.cz/en/financial-markets/foreign-exchange-market/central-bank-exchange-rate-fixing/central-bank-exchange-rate-fixing/daily.txt', // use backend server
         {headers: headers});
     return await response.text();
 };
